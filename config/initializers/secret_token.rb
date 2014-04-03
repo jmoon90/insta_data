c073cde10d5e17b1f8176715d1278625beb0d949
John Moon <johnmoonyy@gmail.com>
2014-04-03 14:48:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InstaData::Application.config.secret_key_base = '780dccde363aa12eafb83d8774617b4b02e614fa96b41a0d2de55bce0a512baf7d71b0c2873ed4c130a9a3670bcc28af74662935ca2d774b98c5940e4e549d76'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InstaData::Application.config.session_store :cookie_store, key: '_insta_data_session'
